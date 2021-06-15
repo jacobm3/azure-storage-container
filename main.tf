5c5a5706f48656a903024c7aae50c94154fc821b
Jacob <jacobm3@gmail.com>
2021-06-14 20:19:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "demo" {
 }
 
 resource "azurerm_storage_account" "demo" {
-  name                     = "jmartinson-storacct"
+  name                     = "jmartinsonstoracct"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
