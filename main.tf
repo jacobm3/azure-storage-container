d07f218ee8d88fbf372df4cec54a9277f8a9e6cd
Jacob <jacobm3@gmail.com>
2021-06-14 20:12:18 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "demo" {
+  name     = "jmartinson-demo2021"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "demo" {
+  name                     = "jmartinson-storacct"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "jmartinson01" {
+  name                  = "jmartinson-container-01"
+  storage_account_name  = azurerm_storage_account.demo.name
+  container_access_type = "public"
+}
