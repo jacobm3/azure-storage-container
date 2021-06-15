26500a70eead5d73d558bb1c020bc1d23cc4ba1a
Jacob <jacobm3@gmail.com>
2021-06-14 20:28:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,13 +21,4 @@ resource "azurerm_storage_container" "demo" {
   container_access_type = "container"
 }
 
-resource "azurerm_storage_blob" "jmartinson01-demosb" {
-  name = "jmartinson01-demosb"
 
-  resource_group_name    = azurerm_resource_group.demo.name
-  storage_account_name   = azurerm_storage_account.demo.name
-  storage_container_name = azurerm_storage_container.demo.name
-
-  type = "page"
-  size = 5120
-}
